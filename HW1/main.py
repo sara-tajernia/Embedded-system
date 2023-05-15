b1762077084e1bf91e4dc4d6ad03e6ea2cbfab54
sara_tjn <tajernia.sara@gmail.com>
2023-05-16 01:36:26 +0800

    add plot

--- HW1/main.py
+++ HW1/main.py
@@ -1,15 +1,17 @@
 from task_set import TaskSet
 from scheduler import Scheduler
-
+from printer import Printer
 
 
 if __name__ == '__main__':
     # task_set = read_tasks_from_csv('tasks1.csv')
-    task_set = TaskSet('tasks1.csv')
-    # task_set = TaskSet('tasks_interrupts.csv')
-    # Scheduler(task_set, 'EDF_preemptive')
+    # task_set = TaskSet('tasks1.csv')
+    task_set = TaskSet('tasks_interrupts.csv')
+    Scheduler(task_set, 'EDF_preemptive')
     # Scheduler(task_set, 'RM')
-    Scheduler(task_set, 'EDF_non_preemptive')
+    # Scheduler(task_set, 'EDF_non_preemptive')
+    # Printer(time_table, raise_task, miss_task)
+
 
 
 
